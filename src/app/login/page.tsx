f14c65f91d9180063b5244ff2e1f617ebfde532c
DmitryMishchenko <abbatd@gmail.com>
2024-07-13 21:44:35 +0400

    refactor components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { AuthForm } from "./auth-form/AuthForm";
+import { AuthForm } from "@/components/AuthForm";
 
 export const metadata: Metadata = {
   title: "Login",
